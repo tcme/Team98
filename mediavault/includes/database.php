474d189a2b62405eeb2011743b82bd9bb0f8cd2d
Leon Pearce <leonp1991@gmail.com>
2015-08-21 16:52:48 +1000

    establishing database
    attempt 1…

--- mediavault/includes/database.php
+++ mediavault/includes/database.php
@@ -1,8 +1,8 @@
 <!-- Included php script to connect to the database, include to this should be on every page that utilises the database -->
 
 <?php
-	$dbcon = mysqli_connect ("localhost", "root", "password", "mediavault");
-	
+	$dbcon = mysqli_connect ("localhost", "ec2-user", "password", "mediavault");
+
 	if (mysqli_connect_errno($dbcon))
 	{
 	echo "Unable to connect to the server:" . mysqli_connect_error();
