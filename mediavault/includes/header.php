8db6e8aec1833298805b6bec5aa3e7f080a383ed
Leon Pearce <leonp1991@gmail.com>
2015-08-27 21:31:25 +1000

    styled navigation.php

--- mediavault/includes/header.php
+++ mediavault/includes/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,11 +24,5 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="../js/bootstrap.min.js"></script>
+
 	</head>
-	
-<body>
-	
-	<div class="container">
-		<div id="logo" class="row">
-			<img src="../images/logo.png" class="img-responsive"></img>
-		</div>
