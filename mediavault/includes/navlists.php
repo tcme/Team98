edb4fa473deb29945f9dbe3bec515c3bb57fbc83
Leon Pearce <leonp1991@gmail.com>
2015-09-03 18:29:40 +1000

    nabber touch ups.
    the navber has been fixed up and is hopefully complete.

---
+++ mediavault/includes/navlists.php
@@ -0,0 +1,13 @@
+<!-- list of left oriented navigation links -->
+<ul class='nav navbar-nav'>
+<li><a href='../pages/video.php'>Video</a></li>
+<li><a href='../pages/audio.php'>Audio</a></li>
+<li><a href='../pages/images.php'>Images</a></li>
+<li><a href='../pages/books.php'>Books</a><li>
+<li><a href='../pages/info.php'>Account Info</a></li>
+</ul>
+
+<!-- list of right oriented navigation links -->
+<ul class='nav navbar-nav navbar-right'>
+<li><a href='../pages/logout.php' class='navText'>Logout</a></li>
+</ul>
