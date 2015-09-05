faca197326c6e307778357cfc0ee3c7484fcb2e0
Leon Pearce <leonp1991@gmail.com>
2015-09-05 14:31:45 +1000

    fixed headers in privacy policies

--- mediavault/pages/privacypolicy.php
+++ mediavault/pages/privacypolicy.php
@@ -10,35 +10,35 @@
 
       <h1>Media Vault Privacy Policy</h1>
 
-		<p>This Privacy Policy was last updated on the 01/09/2015.</p>
+			<p>This Privacy Policy was last updated on the 01/09/2015.</p>
 
-		<p>This Page informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from the users from the Media Vault website. We use your Personal Information only for providing and improving the Media Vault website. By using our website, you agree to the following; the collection and use of information in accordance with this policy.</p>
+			<p>This Page informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from the users from the Media Vault website. We use your Personal Information only for providing and improving the Media Vault website. By using our website, you agree to the following; the collection and use of information in accordance with this policy.</p>
 
-		<h2>Information Collection and Use</h2>
+			<h3>Information Collection and Use</h3>
 
-		<p>While using our website, we may ask you to provide us with certain personal identifiable information which may include, but is not limited to your name, so that we can use it to contact or identify you when you access the website.</p>
+			<p>While using our website, we may ask you to provide us with certain personal identifiable information which may include, but is not limited to your name, so that we can use it to contact or identify you when you access the website.</p>
 
-		<h2>Log Data</h2>
+			<h3>Log Data</h3>
 
-		<p>We collect information that the users’ browser sends whenever they access our website. This Log Data may include information such as your computer’s Internet Protocol address, browser type, browser version, the pages of our website that you have visited as well as the time and date of your visit along with the amount of time that you spent on those pages and other statistics.</p>
+			<p>We collect information that the users’ browser sends whenever they access our website. This Log Data may include information such as your computer’s Internet Protocol address, browser type, browser version, the pages of our website that you have visited as well as the time and date of your visit along with the amount of time that you spent on those pages and other statistics.</p>
 
-		<h2>Cookies</h2>
+			<h3>Cookies</h3>
 
-		<p>We use ‘cookies’ to collect information. However, the user can instruct their browser to refuse all cookies or to indicate when a cookie is being sent. Although, if the user does not accept the cookies, parts of our website may be limited to the user.</p>
+			<p>We use ‘cookies’ to collect information. However, the user can instruct their browser to refuse all cookies or to indicate when a cookie is being sent. Although, if the user does not accept the cookies, parts of our website may be limited to the user.</p>
 
-		<h2>Security</h2>
+			<h3>Security</h3>
 
-		<p>The security of the users’ Personal information is important to us, but remember that no method of transmission over the internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect the user’s Personal information, we cannot guarantee its absolute security.</p>
+			<p>The security of the users’ Personal information is important to us, but remember that no method of transmission over the internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect the user’s Personal information, we cannot guarantee its absolute security.</p>
 
-		<h2>Changes to this privacy Policy</h2>
+			<h3>Changes to this privacy Policy</h3>
 
-		<p>The Privacy Policy may change and be updated from time to time. The user will be notified respectively regarding any changes as the updates will be posted through the website. The user is recommended and advised to review the Privacy Policy periodically for any changes that may occur.</p>
+			<p>The Privacy Policy may change and be updated from time to time. The user will be notified respectively regarding any changes as the updates will be posted through the website. The user is recommended and advised to review the Privacy Policy periodically for any changes that may occur.</p>
 
-		<h2>Contact Us</h2>
+			<h3>Contact Us</h3>
 
-		<p>If the user has any further questions regarding our Privacy Policy, please contact us.</p>
+			<p>If the user has any further questions regarding our Privacy Policy, please contact us.</p>
 
-    <a class="button-link" href="login.php" alt="back"><button type="button" class="button form-control">BACK</button></a>
+	    <a class="button-link" href="login.php" alt="back"><button type="button" class="button form-control">BACK</button></a>
 
     </div>
   </div>
