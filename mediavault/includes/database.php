44a56cd8aa9bc336879e6badbfb860256c712252
Leon Pearce <leonp1991@gmail.com>
2015-09-05 14:22:05 +1000

    broke DB again... fixed now :)

--- mediavault/includes/database.php
+++ mediavault/includes/database.php
@@ -1,6 +1,6 @@
 <?php
 	/*Included php script to connect to the database, include to this should be on every page that utilises the database*/
-	$dbcon = mysqli_connect ("localhost", "ec2-user", "Team98" "mediavault");
+	$dbcon = mysqli_connect ("localhost", "ec2-user", "Team98", "mediavault");
 
 	if (mysqli_connect_errno($dbcon)){
 		echo "Unable to connect to the server:" . mysqli_connect_error();
